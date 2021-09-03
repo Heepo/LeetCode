69dbda7a54bd77decd02c2af4acece7efba102c9
heepo <everleu@gmail.com>
2021-09-03 22:22:32 +0800

    updated

--- 912_sort_an_array.cpp
+++ 912_sort_an_array.cpp
@@ -1,14 +1,17 @@
-#include<vector>
+#include <vector>
+#include <random>
+#include <algorithm>
 using namespace std;
 class Solution {
 public:
     vector<int> sortArray(vector<int>& nums) {
-        // merge sort
         if (nums.size() <= 1) {
             return nums;
         }
         
         mergeSort(nums, 0, nums.size());
+        std::shuffle(nums.begin(), nums.end(), nums); 
+        quickSort(nums, 0, nums.size() - 1);
         return nums;
     }
     
@@ -54,6 +57,49 @@ public:
             //std::cout << "left + k: " << left + k << " merged[k]: " << merged[k] << std::endl;
         }
         
+        return;
+    }
+
+    void quickSort(vector<int>& nums, int start, int end) {
+        if (start >= end) {
+            return;
+        }
+        
+        int pivot = partition(nums, start, end);
+        quickSort(nums, start, pivot - 1);
+        quickSort(nums, pivot + 1, end);
+    }
+    
+    // 5 3 2 5 6 2 9 1
+    // 5 3 2 5 (6) 2 9 1 -> 5 3 2 5 (2) 6 9 1
+    // 5 3 2 5 2 (6) 9 1 -> 5 3 2 5 2 (1) 9 6
+    // 5 3 2 5 2 1 9 6 -> 1 3 2 5 2 | 5 | 9 6
+    int partition(vector<int>& nums, int start, int end) {
+        int pivot = nums[start];
+        int first_larger_index = end + 1;
+        for (int i = start; i <= end; i++) {
+            if (first_larger_index == end + 1 && nums[i] > pivot) {
+                first_larger_index = i;
+                break;
+            }
+        }
+        
+        for (int i = first_larger_index; i <= end; i++) {
+            if (nums[i] < pivot) {
+                swap(nums, first_larger_index, i);
+                first_larger_index++;
+            }
+        }
+        
+        swap(nums, start, first_larger_index - 1);
+        
+        return first_larger_index - 1;
+    }
+    
+    void swap(vector<int>& nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
         return;
     }
 };
